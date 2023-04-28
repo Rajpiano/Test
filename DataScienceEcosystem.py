bfd83009587506c6fee901cd043f15ca19c7b69e
Rajpiano <132068738+Rajpiano@users.noreply.github.com>
2023-04-29 05:25:13 +0530

    Add files via upload
    Github file for Assessment IBM

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# #### In this notebook, Data Science Tools and Ecosystem are summarized
+
+# **Objectives:**
+
+# * R
+# * Python
+# * SQL
+# * Java
+# * Julia
+
+# Some of the popular languages that Data Scientists use are:
+
+# 1) R
+# 2) Python
+# 3) Sql
+# 4) Java
+# 5) Julia
+
+# 1) NumPy
+# 2) Pandas
+# 3) Matplotlib
+# 4) Keras
+# 5) Scikit-Learn
+# 6) Dplyr
+# 7) GGplot
+
+# |***Data Sciense Tools***|
+# |-|
+# |R Studio|
+# |Jupyter Notebook|
+# |PyCharm|
+# |Visual Studio Code|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[8]:
+
+
+#This a simple arithmetic expression to mutiply then add integers. 
+(3*4)+5
+
+
+# In[9]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# ## Author
+
+# Rajaneesh Anidil Pathikkaran
+
+# In[ ]:
+
+
+
+
